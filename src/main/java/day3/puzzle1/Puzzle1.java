db03de7fcb189add0d5c63899806e520297a9721
GEOFFROY Kirsan-Prestataire <kirsan.geoffroy-ext@pole-emploi.fr>
2024-12-08 20:48:00 +0100

    refacto: clean code

--- src/main/java/day3/puzzle1/Puzzle1.java
+++ src/main/java/day3/puzzle1/Puzzle1.java
@@ -2,31 +2,26 @@ package src.main.java.day3.puzzle1;
 
 import src.main.java.Utils;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
 import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class Puzzle1 {
 
-    private static final String REGEX = "mul\\(\\d+,\\d+\\)";
+    private static final String REGEX_MUL = "mul\\(\\d+,\\d+\\)"; // Permet de récupérer les "mul(u,v)mul(x,y)..."
+    private static final String REGEX_DIGIT = "mul\\((\\d+),(\\d+)\\)"; // Permet de récupérer les nombres
+    private static final String FILENAME = "src/main/java/day3/puzzle1/input.txt";
 
     public static void main(String[] args) throws IOException {
-        String fileName = "src/main/java/day3/puzzle1/input.txt";
         Integer result = 0;
-        List<String> lines = Utils.readFile(fileName);
+        List<String> lines = Utils.readFile(FILENAME);
         for(String line: lines){
-            Pattern pattern = Pattern.compile(REGEX);
-            Matcher matcher = pattern.matcher(line);
-            while (matcher.find()) {
-                String regexDigit = "mul\\((\\d+),(\\d+)\\)";
-                Pattern patternDigit = Pattern.compile(regexDigit);
-                Matcher matcherDigit = patternDigit.matcher(matcher.group());
+            Pattern patternMul = Pattern.compile(REGEX_MUL);
+            Matcher matcherMul = patternMul.matcher(line);
+            while (matcherMul.find()) {
+                Pattern patternDigit = Pattern.compile(REGEX_DIGIT);
+                Matcher matcherDigit = patternDigit.matcher(matcherMul.group());
                 if (matcherDigit.matches()) {
                     int num1 = Integer.parseInt(matcherDigit.group(1));
                     int num2 = Integer.parseInt(matcherDigit.group(2));
